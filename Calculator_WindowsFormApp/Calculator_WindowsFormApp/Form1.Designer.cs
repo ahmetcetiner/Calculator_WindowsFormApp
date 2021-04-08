4558c1b5dcbd87f703c062bca0c4c3e6797f87f8
ahmetcetiner <66512698+ahmetcetiner@users.noreply.github.com>
2021-04-08 15:59:58 +0300

    Feat: Form desing add

--- Calculator_WindowsFormApp/Calculator_WindowsFormApp/Form1.Designer.cs
+++ Calculator_WindowsFormApp/Calculator_WindowsFormApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator_WindowsFormApp
 {
-    partial class Form1
+    partial class frm_main
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -28,13 +28,330 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_pls = new System.Windows.Forms.Button();
+            this.btn_min = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_times = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_divided = new System.Windows.Forms.Button();
+            this.btn_0 = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.btn_equals = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_1
+            // 
+            this.btn_1.BackColor = System.Drawing.Color.Black;
+            this.btn_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1.ForeColor = System.Drawing.Color.White;
+            this.btn_1.Location = new System.Drawing.Point(10, 175);
+            this.btn_1.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(65, 35);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.Text = "1";
+            this.btn_1.UseVisualStyleBackColor = false;
+            // 
+            // btn_2
+            // 
+            this.btn_2.BackColor = System.Drawing.Color.Black;
+            this.btn_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2.ForeColor = System.Drawing.Color.White;
+            this.btn_2.Location = new System.Drawing.Point(85, 175);
+            this.btn_2.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(65, 35);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = false;
+            // 
+            // btn_3
+            // 
+            this.btn_3.BackColor = System.Drawing.Color.Black;
+            this.btn_3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_3.ForeColor = System.Drawing.Color.White;
+            this.btn_3.Location = new System.Drawing.Point(160, 175);
+            this.btn_3.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(65, 35);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = false;
+            // 
+            // btn_pls
+            // 
+            this.btn_pls.BackColor = System.Drawing.Color.Black;
+            this.btn_pls.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_pls.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_pls.ForeColor = System.Drawing.Color.White;
+            this.btn_pls.Location = new System.Drawing.Point(10, 130);
+            this.btn_pls.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_pls.Name = "btn_pls";
+            this.btn_pls.Size = new System.Drawing.Size(65, 35);
+            this.btn_pls.TabIndex = 3;
+            this.btn_pls.Text = "+";
+            this.btn_pls.UseVisualStyleBackColor = false;
+            // 
+            // btn_min
+            // 
+            this.btn_min.BackColor = System.Drawing.Color.Black;
+            this.btn_min.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_min.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_min.ForeColor = System.Drawing.Color.White;
+            this.btn_min.Location = new System.Drawing.Point(85, 130);
+            this.btn_min.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_min.Name = "btn_min";
+            this.btn_min.Size = new System.Drawing.Size(65, 35);
+            this.btn_min.TabIndex = 7;
+            this.btn_min.Text = "-";
+            this.btn_min.UseVisualStyleBackColor = false;
+            // 
+            // btn_6
+            // 
+            this.btn_6.BackColor = System.Drawing.Color.Black;
+            this.btn_6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_6.ForeColor = System.Drawing.Color.White;
+            this.btn_6.Location = new System.Drawing.Point(160, 220);
+            this.btn_6.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(65, 35);
+            this.btn_6.TabIndex = 6;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = false;
+            // 
+            // btn_5
+            // 
+            this.btn_5.BackColor = System.Drawing.Color.Black;
+            this.btn_5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_5.ForeColor = System.Drawing.Color.White;
+            this.btn_5.Location = new System.Drawing.Point(85, 220);
+            this.btn_5.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(65, 35);
+            this.btn_5.TabIndex = 5;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = false;
+            // 
+            // btn_4
+            // 
+            this.btn_4.BackColor = System.Drawing.Color.Black;
+            this.btn_4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_4.ForeColor = System.Drawing.Color.White;
+            this.btn_4.Location = new System.Drawing.Point(10, 220);
+            this.btn_4.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(65, 35);
+            this.btn_4.TabIndex = 4;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = false;
+            // 
+            // btn_times
+            // 
+            this.btn_times.BackColor = System.Drawing.Color.Black;
+            this.btn_times.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_times.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_times.ForeColor = System.Drawing.Color.White;
+            this.btn_times.Location = new System.Drawing.Point(160, 130);
+            this.btn_times.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_times.Name = "btn_times";
+            this.btn_times.Size = new System.Drawing.Size(65, 35);
+            this.btn_times.TabIndex = 11;
+            this.btn_times.Text = "*";
+            this.btn_times.UseVisualStyleBackColor = false;
+            // 
+            // btn_9
+            // 
+            this.btn_9.BackColor = System.Drawing.Color.Black;
+            this.btn_9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_9.ForeColor = System.Drawing.Color.White;
+            this.btn_9.Location = new System.Drawing.Point(160, 265);
+            this.btn_9.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(65, 35);
+            this.btn_9.TabIndex = 10;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = false;
+            // 
+            // btn_8
+            // 
+            this.btn_8.BackColor = System.Drawing.Color.Black;
+            this.btn_8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_8.ForeColor = System.Drawing.Color.White;
+            this.btn_8.Location = new System.Drawing.Point(85, 265);
+            this.btn_8.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(65, 35);
+            this.btn_8.TabIndex = 9;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = false;
+            // 
+            // btn_7
+            // 
+            this.btn_7.BackColor = System.Drawing.Color.Black;
+            this.btn_7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_7.ForeColor = System.Drawing.Color.White;
+            this.btn_7.Location = new System.Drawing.Point(10, 265);
+            this.btn_7.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(65, 35);
+            this.btn_7.TabIndex = 8;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = false;
+            // 
+            // btn_divided
+            // 
+            this.btn_divided.BackColor = System.Drawing.Color.Black;
+            this.btn_divided.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_divided.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_divided.ForeColor = System.Drawing.Color.White;
+            this.btn_divided.Location = new System.Drawing.Point(235, 130);
+            this.btn_divided.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_divided.Name = "btn_divided";
+            this.btn_divided.Size = new System.Drawing.Size(65, 35);
+            this.btn_divided.TabIndex = 15;
+            this.btn_divided.Text = "/";
+            this.btn_divided.UseVisualStyleBackColor = false;
+            // 
+            // btn_0
+            // 
+            this.btn_0.BackColor = System.Drawing.Color.Black;
+            this.btn_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0.ForeColor = System.Drawing.Color.White;
+            this.btn_0.Location = new System.Drawing.Point(85, 310);
+            this.btn_0.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_0.Name = "btn_0";
+            this.btn_0.Size = new System.Drawing.Size(65, 35);
+            this.btn_0.TabIndex = 13;
+            this.btn_0.Text = "0";
+            this.btn_0.UseVisualStyleBackColor = false;
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.BackColor = System.Drawing.Color.Black;
+            this.btn_Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Clear.ForeColor = System.Drawing.Color.White;
+            this.btn_Clear.Location = new System.Drawing.Point(235, 175);
+            this.btn_Clear.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(65, 35);
+            this.btn_Clear.TabIndex = 16;
+            this.btn_Clear.Text = "C";
+            this.btn_Clear.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(235, 220);
+            this.button2.Margin = new System.Windows.Forms.Padding(5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 35);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "<--";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(290, 75);
+            this.textBox1.TabIndex = 18;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Black;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.Location = new System.Drawing.Point(270, 2);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(30, 20);
+            this.btn_Close.TabIndex = 19;
+            this.btn_Close.Text = "X";
+            this.btn_Close.UseVisualStyleBackColor = false;
+            // 
+            // btn_equals
+            // 
+            this.btn_equals.BackColor = System.Drawing.Color.Black;
+            this.btn_equals.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_equals.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_equals.ForeColor = System.Drawing.Color.White;
+            this.btn_equals.Location = new System.Drawing.Point(235, 265);
+            this.btn_equals.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_equals.Name = "btn_equals";
+            this.btn_equals.Size = new System.Drawing.Size(65, 35);
+            this.btn_equals.TabIndex = 20;
+            this.btn_equals.Text = "=";
+            this.btn_equals.UseVisualStyleBackColor = false;
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(310, 355);
+            this.Controls.Add(this.btn_equals);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_divided);
+            this.Controls.Add(this.btn_0);
+            this.Controls.Add(this.btn_times);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.btn_min);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.btn_pls);
+            this.Controls.Add(this.btn_3);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_main";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Button btn_pls;
+        private System.Windows.Forms.Button btn_min;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Button btn_times;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Button btn_divided;
+        private System.Windows.Forms.Button btn_0;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_equals;
     }
 }
 
--- Calculator_WindowsFormApp/Calculator_WindowsFormApp/Form1.cs
+++ Calculator_WindowsFormApp/Calculator_WindowsFormApp/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Calculator_WindowsFormApp
 {
-    public partial class Form1 : Form
+    public partial class frm_main : Form
     {
-        public Form1()
+        public frm_main()
         {
             InitializeComponent();
         }
